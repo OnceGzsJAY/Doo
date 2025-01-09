252620d3eb17861866e37fa04f9b3c2cef579da4
OnceGzsJAY <guozisheng2022@126.com>
2025-01-09 21:18:41 +0800

    fix the script, from callback to asynchronous

--- core/http/post.js
+++ core/http/post.js
@@ -1,38 +1,39 @@
-function http_post(url, post_content, request_header = ["Content-Type", "application/json"], timeout_time = 2000 /* ms */, true_status_code = 200, callback) {
-    const http_connect = {
-        request: new XMLHttpRequest(),
-        return_value: []
-    };
-
-    http_connect.request.open("POST", url, true); // connect
+function http_post(url, post_content, request_header = ["Content-Type", "application/json"], timeout_time = 2000 /* ms */, true_status_code = 200) { 
+    return new Promise((resolve, reject) => {
+        const http_connect = {
+            request: new XMLHttpRequest(),
+            // return_value: []
+        };
     
-    // init header
-    request_header.forEach(header => {
-        http_connect.request.setRequestHeader(header[0], header[1]);
-    });
-    // init header
-
-    // request timeout
-    http_connect.request.timeout = timeout_time;
-    http_connect.request.ontimeout = function () {
-        http_connect.return_value.push("timeout!");
-        callback(http_connect.return_value);
-    };
-
-    // request answer
-    http_connect.request.onreadystatechange = function () {
-        if (http_connect.request.readyState === 4) { // request ok
-            if (http_connect.request.status === true_status_code) { // check status
-                http_connect.return_value.push(http_connect.request.responseText);
-                callback(http_connect.return_value);
-            } else {
-                http_connect.return_value.push("status is bad", http_connect.request.responseText);
-                callback(http_connect.return_value);
+        http_connect.request.open("POST", url, true); // connect
+        
+        // init header
+        request_header.forEach(header => {
+            http_connect.request.setRequestHeader(header[0], header[1]);
+        });
+        // init header
+    
+        // request timeout
+        http_connect.request.timeout = timeout_time;
+        http_connect.request.ontimeout = function () {
+            http_connect.return_value.push("timeout!");
+        };
+        // request timeout
+    
+        // request answer
+        http_connect.request.onreadystatechange = function () {
+            if (http_connect.request.readyState === 4) { // request ok
+                if (http_connect.request.status === true_status_code) { // check status
+                    resolve.return_value.push(http_connect.request.responseText);
+                } else {
+                    reject("status is bad. the responseText : " + http_connect.request.responseText);
+                }
             }
-        }
-    };
-
-    http_connect.request.send(post_content);
+        };
+        // request answer
+    
+        http_connect.request.send(post_content);
+    })
 }
 
 export { http_post }
