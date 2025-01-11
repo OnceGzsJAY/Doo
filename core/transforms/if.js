9c11264c5ecbbb122b8800f45720466095de5c6e
OnceGzsJAY <guozisheng2022@126.com>
2025-01-11 18:18:03 +0800

    add the if of transforms module

---
+++ core/transforms/if.js
@@ -0,0 +1,11 @@
+function doo_if(expression ,code ,different_id) {
+    the_expression_code = eval(expression);
+    let file_id_object = document.getElementById(different_id);
+    if (the_expression_code) {
+        file_id_object.innerHTML(code);
+        return true;
+    }else {
+        return false;
+    }
+    return "Error : please email the Doo Team";
+}
