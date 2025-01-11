798653496b96d7e9f3f5516e6d1ab57e16737dc6
OnceGzsJAY <guozisheng2022@126.com>
2025-01-11 21:34:58 +0800

    fix if.js

--- core/transforms/if.js
+++ core/transforms/if.js
@@ -1,13 +1,11 @@
 function doo_if(expression ,code ,id) {
-    the_expression_code = eval(expression);
+    let the_expression_code = eval(expression);
     let file_id_object = document.getElementById(id);
     if (the_expression_code) {
-        file_id_object.innerHTML(code);
+        file_id_object.innerHTML = code;
         return true;
     }else {
         return false;
     }
-    return "Error : please email the Doo Team";
 }
-
 export { doo_if }
