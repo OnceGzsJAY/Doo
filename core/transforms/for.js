19589ea09cd6cef719f2aa2bb0e017c2230f8e86
OnceGzsJAY <guozisheng2022@126.com>
2025-01-11 22:10:30 +0800

    fix for.js

--- core/transforms/for.js
+++ core/transforms/for.js
@@ -1,6 +1,13 @@
-function doo_for (min_value ,max_value ,step = 1 ,code) {
-    for (idx = min_value ; idx <= max_value ; idx += step) {
-        eval(code);
+function doo_for (min_value ,max_value ,step = 1 ,id ,code) {
+    if (typeof min_value !== 'number' || typeof max_value !== 'number' || typeof step !== 'number') {
+        throw new Error('min_value, max_value, and step must be numbers'); // error
+    }
+    if (typeof code !== 'string') {
+        code = String(code); // type to string
+    }
+    let document_object = document.getElementById(id);
+    for (let idx = min_value; idx <= max_value; idx += step) {
+        document_object.innerHTML += code;
     }
 }
 
